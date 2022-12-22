20709bed1a851510ac0db37f48b7c4821b46e41b
Egon Rath <egon.rath@gmail.com>
2022-12-22 08:32:26 +0100

    added a shell script to ease building

---
+++ build_example.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#
+# Example of how to build the SDL2 version of Mini vMac
+#
+
+# we need to build the setup tool first
+if [ ! -x ./setup_t ]; then
+	gcc -o setup_t setup/tool.c
+fi
+
+# run setup tool to generate makefile generator
+./setup_t -maintainer "egon.rath@gmail.com" \
+        -homepage "https://github.com/egrath" \
+        -n "minivmac-3.7-test" \
+        -e bgc \
+        -t lx64 \
+        -m II \
+        -hres 640 -vres 480 -depth 3 \
+        -magnify 1 \
+        -mf 2 \
+        -sound 1 \
+        -sony-sum 1 -sony-tag 1 \
+        -speed 4 -ta 2 -em-cpu 2 -mem 8M \
+        -chr 0 -drc 1 -sss 4 \
+        -fullscreen 0 \
+        -var-fullscreen 1 \
+        -api sd2 \
+        > setup.sh
+
+# generate makefile and build
+bash -x ./setup.sh
+make clean
+make -j $(nproc)
+
